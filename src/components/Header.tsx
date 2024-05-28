edbd885529056ad2fb756d433c14204a2bfc88ea
Thomas Viles <thomas@blackwidowimaging.com>
2024-05-28 09:20:26 -0400

    do some ui work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Grid, Menu, MenuItem, Button, IconButton } from '@mui/material';
-import { tss } from 'tss-react'
-import MenuIcon from '@mui/icons-material/Menu'
+import { tss } from 'tss-react';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const useStyles = tss.create({
     headerContainer: {
         height: '14vh',
         width: '100vw !important',
         position: 'fixed',
-        background: 'white'
+        background: 'white',
     },
     servingYou: {
         fontFamily: 'Caveat, cursive',
@@ -16,7 +16,7 @@ const useStyles = tss.create({
         background: '#999',
         color: 'white',
         height: '3vh',
-        textAlign: 'center'
+        textAlign: 'center',
     },
     imageContainer: {
         maxWidth: '100%',
@@ -34,15 +34,10 @@ const useStyles = tss.create({
         "@media (min-width: 1000px)": {
             display: "none",
         },
-        '& img': {
-            maxWidth: '100%',
-            maxHeight: '13vh',
-            height: 'auto',
-        },
     },
     linkButtons: {
         color: 'black',
-        padding: '5px'
+        padding: '5px',
     },
     links: {
         color: 'black',
@@ -67,90 +62,83 @@ const useStyles = tss.create({
             display: "none",
         },
     }
-})
+});
 
-export type page = {
+export type Page = {
     title: string,
     href: string
-}
+};
 
-const pages: page[] = [
+const pages: Page[] = [
     { title: 'Resume', href: '/resume' },
-    { title: 'Portfolio', href: '/portfolio' }
-]
+    { title: 'Portfolio', href: '/portfolio' },
+];
 
 function Header() {
-    const { classes } = useStyles()
+    const { classes } = useStyles();
 
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget)
-    }
+        setAnchorElNav(event.currentTarget);
+    };
 
     const handleCloseNavMenu = () => {
-        setAnchorElNav(null)
-    }
+        setAnchorElNav(null);
+    };
 
     return (
         <Grid container className={classes.headerContainer}>
-            <Grid item xs={12} className={classes.servingYou}>Serving your family these fingers since 1993</Grid>
-            <Grid container justifyContent='space-between' alignContent='center'>
-                <Grid container xs={12} sm={8} justifyContent='space-between' alignContent='center'>
-                    <Grid item xs={4} md={5} className={classes.menuContainer}>
-                        <IconButton
-                            size="large"
-                            onClick={handleOpenNavMenu}
+            <Grid container item xs={12} sm={8} justifyContent='space-between' alignContent='center'>
+                <Grid item xs={4} md={5} className={classes.menuContainer}>
+                    <IconButton size="large" onClick={handleOpenNavMenu}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                                <Button
+                                    key={page.title}
+                                    onClick={handleCloseNavMenu}
+                                    className={classes.linkButtons}
+                                >
+                                    {page.title}
+                                </Button>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Grid>
+                <Grid item xs={6} className={classes.imageContainer}>
+                    <img src={require('../media/Rietow DDS JPG.jpg')} alt='' />
+                </Grid>
+            </Grid>
+            <Grid container item xs={6} className={classes.links}>
+                {pages.map((page) => (
+                    <Grid item key={page.title}>
+                        <Button
+                            key={page.title}
+                            onClick={handleCloseNavMenu}
+                            className={classes.linkButtons}
                         >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={() => handleCloseNavMenu()}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                                    <Button
-                                        key={page.title}
-                                        onClick={handleCloseNavMenu}
-                                        className={classes.linkButtons}
-                                    >
-                                        {page.title}
-                                    </Button>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                            {page.title}
+                        </Button>
                     </Grid>
-                    <Grid item xs={6} className={classes.imageContainer}>
-                        <img src={require('../media/Rietow DDS JPG.jpg')} alt='' />
-                    </Grid>
-                </Grid>
-                <Grid container xs={6} className={classes.links}>
-                    {pages.map((page) => (
-                        <Grid>
-                            <Button
-                                key={page.title}
-                                onClick={handleCloseNavMenu}
-                                className={classes.linkButtons}
-                            >
-                                {page.title}
-                            </Button>
-                        </Grid>
-                    ))}
-                </Grid>
-                <Grid item xs={2} sm={4} className={classes.contact}>
-                    <Grid item className={classes.innerContact}>
-                    </Grid>
-                </Grid>
+                ))}
+            </Grid>
+            <Grid item xs={2} sm={4} className={classes.contact}>
+                <Grid item className={classes.innerContact}></Grid>
             </Grid>
         </Grid>
     );
